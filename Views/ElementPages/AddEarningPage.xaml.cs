681965f6ae923f3c591a562dcc56f740d251c853
Andrii <kudenko4614@gmail.com>
2023-04-05 01:17:32 -0400

    Modify addin pages for Earnings/Expenses

--- Views/ElementPages/AddEarningPage.xaml.cs
+++ Views/ElementPages/AddEarningPage.xaml.cs
@@ -6,4 +6,19 @@ public partial class AddEarningPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void AmountEntry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        if (string.IsNullOrEmpty(AmountEntry.Text))
+        {
+            // The Entry control does not contain any text
+            PlusLabel.TextColor = Colors.DimGray;
+
+        }
+        else
+        {
+            // The Entry control contains text
+            PlusLabel.TextColor = Colors.Green;
+        }
+    }
 }
--- Views/ElementPages/AddExpensePage.xaml.cs
+++ Views/ElementPages/AddExpensePage.xaml.cs
@@ -6,4 +6,20 @@ public partial class AddExpensePage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void AmountEntry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        if (string.IsNullOrEmpty(AmountEntry.Text))
+        {
+            // The Entry control does not contain any text
+            PlusLabel.TextColor = Colors.DimGray;
+
+        }
+        else
+        {
+            // The Entry control contains text
+            PlusLabel.TextColor = Colors.Red;
+        }
+
+    }
 }
